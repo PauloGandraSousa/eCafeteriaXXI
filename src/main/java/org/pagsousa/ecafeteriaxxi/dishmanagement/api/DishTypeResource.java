1b8e533417d6b3d29e256d741023e5670b8d7a43
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2023-07-12 20:19:52 +0100

    toogle dish type state

--- src/main/java/org/pagsousa/ecafeteriaxxi/dishmanagement/api/DishTypeResource.java
+++ src/main/java/org/pagsousa/ecafeteriaxxi/dishmanagement/api/DishTypeResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +44,6 @@ import lombok.RequiredArgsConstructor;
 public class DishTypeResource {
 
 	private final DishTypeService service;
-
 	private final DishTypeViewMapper viewMapper;
 
 	private Long getVersionFromIfMatchHeader(final String ifMatchHeader) {
@@ -128,5 +128,19 @@ public class DishTypeResource {
 
 		// TODO check if we can distinguish between a 404 and a 412
 		return count == 1 ? ResponseEntity.noContent().build() : ResponseEntity.status(412).build();
+	}
+
+	@Operation(summary = "Toogles the state of an existing dish type")
+	@PostMapping("/{acronym}/state")
+	public ResponseEntity<DishTypeView> toogleState(final WebRequest request,
+			@PathVariable("acronym") @Parameter(description = "The acronym of the dish type to update") final String acronym,
+			@Valid @RequestBody final ToogleStateRequest resource) {
+		final var ifMatchValue = request.getHeader("If-Match");
+		if (ifMatchValue == null || ifMatchValue.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"You must issue a conditional POST using 'if-match'");
+		}
+		final var dt = service.toogleState(acronym, getVersionFromIfMatchHeader(ifMatchValue));
+		return ResponseEntity.ok().eTag(Long.toString(dt.getVersion())).body(viewMapper.toView(dt));
 	}
 }
---
+++ src/main/java/org/pagsousa/ecafeteriaxxi/dishmanagement/api/ToogleStateRequest.java
@@ -0,0 +1,10 @@
+package org.pagsousa.ecafeteriaxxi.dishmanagement.api;
+
+/**
+ *
+ * @author Paulo Gandra Sousa 12/07/2023.
+ *
+ */
+public class ToogleStateRequest {
+//empty request body
+}
