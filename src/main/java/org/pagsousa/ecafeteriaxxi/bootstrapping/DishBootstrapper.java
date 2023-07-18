ceee32161911ba7adfbf5e94ab5dd65beaef11d5
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2023-07-18 18:00:04 +0100

    dish bootstrapper

--- src/main/java/org/pagsousa/ecafeteriaxxi/bootstrapping/DishBootstrapper.java
+++ src/main/java/org/pagsousa/ecafeteriaxxi/bootstrapping/DishBootstrapper.java
@@ -1,7 +1,9 @@
 package org.pagsousa.ecafeteriaxxi.bootstrapping;
 
+import org.pagsousa.ecafeteriaxxi.dishmanagement.domain.model.Dish;
 import org.pagsousa.ecafeteriaxxi.dishmanagement.domain.model.DishType;
 import org.pagsousa.ecafeteriaxxi.dishmanagement.domain.model.DishTypeAcronym;
+import org.pagsousa.ecafeteriaxxi.dishmanagement.domain.repositories.DishRepository;
 import org.pagsousa.ecafeteriaxxi.dishmanagement.domain.repositories.DishTypeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import eapli.framework.general.domain.model.Description;
+import eapli.framework.general.domain.model.Designation;
+import eapli.framework.money.domain.model.Money;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -27,19 +31,34 @@ import lombok.RequiredArgsConstructor;
 @Profile("ecafeteriaxxi.bootstrap")
 public class DishBootstrapper implements CommandLineRunner {
 
-	private final DishTypeRepository repo;
+	private final DishTypeRepository dishTypeRepo;
+	private final DishRepository dishRepo;
 
 	@Override
 	@Transactional
 	public void run(final String... args) throws Exception {
-		if (repo.findByAcronym(DishTypeAcronym.valueOf("peixe")).isEmpty()) {
-			final var u1 = new DishType(DishTypeAcronym.valueOf("peixe"), Description.valueOf("pratos do mar"));
-			repo.save(u1);
-		}
+		final var peixe = dishTypeRepo.findByAcronym(DishTypeAcronym.valueOf("peixe")).orElseThrow();
+		final var carne = dishTypeRepo.findByAcronym(DishTypeAcronym.valueOf("carne")).orElseThrow();
 
-		if (repo.findByAcronym(DishTypeAcronym.valueOf("carne")).isEmpty()) {
-			final var u1 = new DishType(DishTypeAcronym.valueOf("carne"), Description.valueOf("pratos da terra"));
-			repo.save(u1);
-		}
+		var d = createDish("Bacalhau à Bráz", "Receita que não sabemos bem qual o nome mas é deliciosa.", peixe,
+				Money.euros(3.99));
+		dishRepo.save(d);
+
+		d = createDish("Bacalhau à Zé do Pipo", "Receita que não sabemos bem qual o nome mas é deliciosa.", peixe,
+				Money.euros(4.99));
+		dishRepo.save(d);
+
+		d = createDish("Costeleta à salsicheiro", "Receita que não sabemos bem qual o nome mas é deliciosa.", carne,
+				Money.euros(2.99));
+		dishRepo.save(d);
+
+		d = createDish("Picanha", "Receita que não sabemos bem qual o nome mas é deliciosa.", carne, Money.euros(5.99));
+		dishRepo.save(d);
+	}
+
+	private Dish createDish(final String dsg, final String desc, final DishType type, final Money price) {
+		final var theDsg = Designation.valueOf(dsg);
+		final var theDesc = Description.valueOf(desc);
+		return new Dish(type, theDsg, price, theDesc);
 	}
 }
